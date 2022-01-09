198ec6f9d95e37f197865c83687119ba6beab7ed
Lucas Sena <ls.lucas96@gmail.com>
2022-01-09 18:31:25 -0300

    Solution

--- problem-1003.js
+++ problem-1003.js
@@ -1,6 +1,6 @@
 // let lines = ['1', '2']; // to test
 
-var a = parseInt(lines.shift());
-var b = parseInt(lines.shift());
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
 
 console.log(`SOMA = ${a+b}`);
